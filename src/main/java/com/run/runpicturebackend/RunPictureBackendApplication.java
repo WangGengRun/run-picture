777b442718854ac955e586c6dbabd696d18ab0ee
王庚润 <3206201719@qq.com>
2026-05-20 12:13:34 +0800

    首次提交

---
+++ src/main/java/com/run/runpicturebackend/RunPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.run.runpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/run/runpicturebackend/RunPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.run.runpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
